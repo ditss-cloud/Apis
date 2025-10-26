8f97b0f9e589287ec06c76ad03bf875b18e1a821
DitssGanteng <ditss.cloud@gmail.com>
2025-10-26 14:29:24 +0700

    Create config.js

---
+++ schema/config.js
@@ -0,0 +1,18 @@
+const options = {
+  name: "DitssApis",
+  developer: "@ditss.cloud",
+  port: 3000,
+  webName: "DitssApis Playground",
+  description: "Simple Base Rest API",
+  favicon:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
+};
+
+const host = {
+  BASE_URL: "https://api.ditss.biz.id", 
+};
+
+module.exports = {
+  options,
+  host,
+};
